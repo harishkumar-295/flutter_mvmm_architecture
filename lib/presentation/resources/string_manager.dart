683c0ef3eb5dd2e0c80b4ff7698534666de3301b
harishkumar-295 <coolboyghkumar295@gmail.com>
2023-01-24 21:26:26 +0530

    completed onboarding design

--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,3 +1,16 @@
 class AppString {
   static const noRouteFound = "No Route Found";
+  static const onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const onBoardingTitle4 = "SEE THE BEST COURSE #4";
+  static const onBoardingSubTitle1 =
+      "This is an awesome flutter application using clean architecture #1";
+  static const onBoardingSubTitle2 =
+      "This is an awesome flutter application using clean architecture #2";
+  static const onBoardingSubTitle3 =
+      "This is an awesome flutter application using clean architecture #3";
+  static const onBoardingSubTitle4 =
+      "This is an awesome flutter application using clean architecture #4";
+  static const skip = "skip";
 }
