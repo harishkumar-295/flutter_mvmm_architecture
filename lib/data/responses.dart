998e49de58d17f96f971fb693d0d6d8e74aa7959
harishkumar-295 <coolboyghkumar295@gmail.com>
2023-01-29 14:40:21 +0530

    data domain layers in progress

---
+++ lib/data/responses.dart
@@ -0,0 +1,40 @@
+import "package:json_annotation/json_annotation.dart";
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: "status")
+  int? status;
+  @JsonKey(name: "message")
+  String? message;
+}
+
+@JsonSerializable()
+class CustomerResponse {
+  @JsonKey(name: "id")
+  int? id;
+  @JsonKey(name: "name")
+  String? name;
+  @JsonKey(name: "numOfNotifications")
+  int? numOfNotifications;
+  CustomerResponse(this.id, this.name, this.numOfNotifications);
+}
+
+@JsonSerializable()
+class ContactsResponse {
+  @JsonKey(name: "phone")
+  String? phone;
+  @JsonKey(name: "link")
+  String? link;
+  @JsonKey(name: "email")
+  String? email;
+  ContactsResponse(this.phone, this.link, this.email);
+}
+
+@JsonSerializable()
+class AuthenticationResponse extends BaseResponse {
+  @JsonKey(name: "customer")
+  CustomerResponse? customer;
+  @JsonKey(name: "contact")
+  ContactsResponse? contact;
+  AuthenticationResponse(this.customer, this.contact);
+}
